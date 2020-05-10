d749208e645fb3fd833f254e7edd417f13979379
edikurniawanwibowo <edi@medigo.id>
2020-05-10 14:54:08 +0700

    [UPDATE]

--- public/js/app.js
+++ public/js/app.js
@@ -39053,8 +39053,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/edikurniawan/Documents/edikw/niagahoster/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/edikurniawan/Documents/edikw/niagahoster/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/edikurniawan/Documents/edikw/landing_page/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/edikurniawan/Documents/edikw/landing_page/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
